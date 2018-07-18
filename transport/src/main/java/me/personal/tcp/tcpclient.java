82200ab9809ca312e411ce934a85038519ce4054
zy475459736 <yzhong@seu.edu.cn>
2018-07-19 00:30:59 +0800

    tcp-socket

---
+++ transport/src/main/java/me/personal/tcp/tcpclient.java
@@ -0,0 +1,37 @@
+package me.personal.tcp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * TCP协议是面向连接呃通信协议。必须建立发送与接收端的逻辑连接。
+    ● 客户端想服务端发出连接请求
+    ● 服务端向客户端发送一个响应
+    ● 客服端想服务端发送确认信息
+    ● 刻意确保传输数据的安全性
+    ​==TCP与UDP的区分：==UDP只区分发送和接受，但是TCP严格区分客服端和服务器，服务器不会主动连接客户端。在连接通路完成后悔自动建立一个字节流对象（OutputStream，InputStream）
+ * Created by zhongyi on 2018/7/19.
+ */
+public class tcpclient {
+//    传入服务器的IP和端口号，一运行就会去连接
+//    public Socket(String host,int port);
+//    一但连接就会创建对应的输入输出流
+//    public OutputStream getOutputStream();
+//
+//    public InputStream getInputStream();
+    public static void main(String[] args) {
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1",1000);
+            OutputStream out = socket.getOutputStream();
+            out.write(Integer.parseInt("s"));
+            //网络上的流对象可以不用关闭流对象
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ transport/src/main/java/me/personal/tcp/tcpserver.java
@@ -0,0 +1,38 @@
+package me.personal.tcp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by zhongyi on 2018/7/19.
+ */
+public class tcpserver {
+//    服务器为了区分到底是那个客户端连接了，需要拿到客户端的套接字
+//    public ServiceSocket(int port);
+//    public Socket accept();
+//    这个套接字对象是通过IO传过来的
+//    public InputStream getInpurStream();
+//    向客户端回写数据的方法
+//    public OutputStream getOutputStream();
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(1000);
+            Socket socket = server.accept();
+            InputStream in = socket.getInputStream();
+            byte[] data = new byte[1024];
+            int len = in.read(data);
+            System.out.println(new String(data, 0, len));
+            OutputStream out = socket.getOutputStream();
+            out.write(Integer.parseInt("aaa"));
+            socket.close();
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
