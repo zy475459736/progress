9a5d63daf89ae63b27d7c9e83f179caa150948a7
zhongyi <zhongyi@ppdai.com>
2018-07-19 15:13:04 +0800

    simple demo about threadlocal

---
+++ multithread/src/main/java/threadlocal/DemoThreadLocal.java
@@ -0,0 +1,38 @@
+package threadlocal;
+
+/**
+ * Created by zhongyi on 2018/7/19.
+ */
+public class DemoThreadLocal {
+    protected static Class<? extends DemoThreadLocal> demoThreadLocalClass = DemoThreadLocal.class;
+
+    protected static final ThreadLocal<? extends DemoThreadLocal> threadLocal = new ThreadLocal<DemoThreadLocal>() {
+        @Override
+        protected DemoThreadLocal initialValue() {
+            try {
+                return demoThreadLocalClass.newInstance();
+            } catch (Throwable e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+    };
+
+    /**
+     * Override the default RequestContext
+     *
+     * @param clazz
+     */
+    public static void setDemoThreadLocalClass(Class<? extends DemoThreadLocal> clazz) {
+        demoThreadLocalClass = clazz;
+    }
+    /**
+     * Get the current RequestContext
+     *
+     * @return the current RequestContext
+     */
+    public static DemoThreadLocal getCurrentContext() {
+        DemoThreadLocal context = threadLocal.get();
+        return context;
+    }
+}
