f5698df2f64749edc0fbd9f33ecaefc503b7e406
zhongyi <zhongyi@ppdai.com>
2018-08-05 13:48:33 +0800

    front init

--- front/app/Greeter.js
+++ front/app/Greeter.js
@@ -1,6 +1,7 @@
 // Greeter.js
+var config = require('./config.json');
 module.exports = function() {
     var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
+    greet.textContent = config.greetText;
     return greet;
 };
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,7 +1,29 @@
 module.exports = {
+    devtool: 'eval-source-map',//生成source map,方便调试
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/public",//打包后的文件存放的地方
         filename: "bundle.js"//打包后输出文件的文件名
+    },
+    devServer: {//构建本地服务器，监听代码修改并自动刷新显示修改后的结果。
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env", "react"
+                        ]
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
     }
 }
